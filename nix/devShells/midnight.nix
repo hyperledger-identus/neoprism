75f83a217da24d09c2636160356546be822d5992
Pat Losoponkul <patextreme@hotmail.com>
2025-08-19 17:00:30 +0700

    feat: add midnightUp and midnightDown shell scripts to devShell
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/devShells/midnight.nix
+++ nix/devShells/midnight.nix
@@ -2,16 +2,39 @@
 
 let
   rootDir = "$ROOT_DIR";
+  scripts = {
+    midnightUp = pkgs.writeShellApplication {
+      name = "midnightUp";
+      text = ''
+        cd "${rootDir}"
+        docker run -d --rm \
+          --name midnight-proof-server \
+          -p 6300:6300 \
+          midnightnetwork/proof-server:4.0.0 \
+          midnight-proof-server --network testnet
+      '';
+    };
+    midnightDown = pkgs.writeShellApplication {
+      name = "midnightDown";
+      text = ''
+        cd "${rootDir}"
+        docker stop midnight-proof-server
+      '';
+    };
+  };
 in
 pkgs.mkShell {
-  packages = with pkgs; [
-    docker
-    git
-    hurl
-    jq
-    # midnight
-    pkgsInternal.compactc
-  ];
+  packages =
+    with pkgs;
+    [
+      docker
+      git
+      hurl
+      jq
+      # midnight
+      pkgsInternal.compactc
+    ]
+    ++ (builtins.attrValues scripts);
 
   shellHook = ''
     export ROOT_DIR=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
