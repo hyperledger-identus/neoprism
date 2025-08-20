7c86e825cb89b3367ad20dc4d31ecb2d18343d32
Pat Losoponkul <patextreme@hotmail.com>
2025-08-20 17:43:59 +0700

    refactor: move hello.ts to binding and update module loading path
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- lib/did-midnight/src/lib.rs
+++ lib/did-midnight/src/lib.rs
@@ -5,7 +5,7 @@ pub mod error;
 pub mod prelude;
 
 pub fn call_ts_hello(name: &str) -> Result<String, Error> {
-    let module = Module::load("examples/hello.ts")?;
+    let module = Module::load("binding/hello.ts")?;
     let mut runtime = Runtime::new(Default::default())?;
     let handle = runtime.load_module(&module)?;
     let greeting: String = runtime.call_function(Some(&handle), "hello", json_args!(name))?;
