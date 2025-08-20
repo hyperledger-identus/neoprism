6de29dc9a218a3dfdd9ab07a2a88e2d27657d6b6
Pat Losoponkul <patextreme@hotmail.com>
2025-08-20 15:46:37 +0700

    refactor: improve nix packaging and dev shell formatting
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/devShells/midnight.nix
+++ nix/devShells/midnight.nix
@@ -4,20 +4,18 @@ let
   rootDir = "$ROOT_DIR";
 in
 pkgs.mkShell {
-  packages =
-    with pkgs;
-    [
-      docker
-      git
-      hurl
-      jq
-      python313
-      # midnight
-      pkgsInternal.compactc
-      nodejs_22
-      typescript
-      nodePackages.typescript-language-server
-    ];
+  packages = with pkgs; [
+    docker
+    git
+    hurl
+    jq
+    python313
+    # midnight
+    pkgsInternal.compactc
+    nodejs_22
+    typescript
+    nodePackages.typescript-language-server
+  ];
 
   shellHook = ''
     export ROOT_DIR=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
--- nix/packages/neoprism-bin.nix
+++ nix/packages/neoprism-bin.nix
@@ -15,7 +15,22 @@ in
 rustPlatform.buildRustPackage {
   inherit cargoLock;
   name = "neoprism";
-  src = lib.cleanSource ./../..;
+  src = lib.cleanSourceWith {
+    filter = (
+      path: type:
+      let
+        baseName = builtins.baseNameOf path;
+      in
+      !(
+        baseName == "docs"
+        || baseName == "docker"
+        || baseName == ".github"
+        || baseName == "tests"
+        || baseName == "README.md"
+      )
+    );
+    src = ./../..;
+  };
   nativeBuildInputs = with buildPackages; [ protobuf ];
   doCheck = false;
   PROTOC = "${buildPackages.protobuf}/bin/protoc";
