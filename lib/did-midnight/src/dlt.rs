c88f67ca28b0227476ac9aed8effad7815a4de48
Pat Losoponkul <patextreme@hotmail.com>
2025-08-20 16:44:10 +0700

    feat: add initial did:midnight crate with stubs and config
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

---
+++ lib/did-midnight/src/dlt.rs
@@ -0,0 +1,1 @@
+// Distributed ledger logic for did:midnight (stub)
---
+++ lib/did-midnight/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod did;
+pub mod utils;
+pub mod dlt;
+pub mod error;
+pub mod prelude;
+
+pub use did::*;
+pub use utils::*;
+pub use dlt::*;
+pub use error::*;
+pub use prelude::*;
