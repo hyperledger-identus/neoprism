e795d665a3a85e77d1f21a85375044d1223b35e1
Pat Losoponkul <patextreme@hotmail.com>
2025-08-20 16:17:27 +0700

    refactor: rename midnight.nix to midnight-did.nix in devShells config
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -20,7 +20,7 @@ in
   default = import ./neoprism.nix { inherit pkgs buildConfig; };
   release = import ./release.nix { inherit pkgs buildConfig; };
   cardano = import ./cardano.nix { inherit pkgs; };
-  midnight = import ./midnight.nix { inherit pkgs; };
+  midnight-did = import ./midnight-did.nix { inherit pkgs; };
   prism-test = import ./prism-test.nix { inherit pkgs; };
   docs = import ./docs.nix { inherit pkgs self; };
 }
