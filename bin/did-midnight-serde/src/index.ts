82ecb8e5a6af3c1ae030eca372c05e00f4a98256
Pat Losoponkul <patextreme@hotmail.com>
2025-08-22 17:20:12 +0700

    refactor: move did-midnight-serde files; update decodeContractState return type
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- bin/did-midnight-serde/src/index.ts
+++ bin/did-midnight-serde/src/index.ts
@@ -2,17 +2,16 @@
 import * as DidContract from "./managed/contract/index.cjs";
 import { ContractState } from "@midnight-ntwrk/ledger";
 import { decodeHex } from "@std/encoding/hex";
+import { DidDocument } from "../../../lib/did-core/bindings/DidDocument.ts";
 
 export function decodeContractState(
   contractStateHex: string,
   networkId: number,
-): DidDocument {
+): string {
   if (contractStateHex.length % 2 !== 0) throw new Error("Hex string must have an even length");
   const buffer = decodeHex(contractStateHex);
   const state = ContractState.deserialize(buffer, networkId);
   const ledger = DidContract.ledger(state.data);
   return "Hello";
 }
-
-type DidDocument = string;
 
