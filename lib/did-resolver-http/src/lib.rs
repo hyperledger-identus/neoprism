6fd28957d412c4cf2d83734b0463fefb8add9460
Pat Losoponkul <patextreme@hotmail.com>
2025-09-11 13:05:54 +0700

    feat: implement dynamic openapi path for did resolver endpoint
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- lib/did-resolver-http/src/lib.rs
+++ lib/did-resolver-http/src/lib.rs
@@ -7,10 +7,13 @@ use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{Json, Router};
 use identus_did_core::{Did, DidResolutionErrorCode, DidResolver, ResolutionResult};
+use utoipa::OpenApi;
+
+const PLACEHOLDER_RESOLVER_PATH: &str = "/placeholder-did-resolver";
 
 pub struct DidResolverHttpBinding {
     pub router: Router<DidResolverStateDyn>,
-    // openapi: utoipa::openapi::OpenApi
+    pub openapi: utoipa::openapi::OpenApi,
 }
 
 #[derive(Clone)]
@@ -19,33 +22,42 @@ pub struct DidResolverStateDyn {
 }
 
 pub fn make_resolver_http_binding(path: &str) -> DidResolverHttpBinding {
-    let router = Router::new().route(path, get(did_resolver_endpoint));
+    let router = Router::new().route(path, get(did_resolver));
 
-    DidResolverHttpBinding { router }
+    #[derive(utoipa::OpenApi)]
+    #[openapi(paths(did_resolver))]
+    struct OpenApiDoc;
+
+    let mut openapi = OpenApiDoc::openapi();
+
+    // Replace the placeholder path with the user-provided path.
+    // This approach allows us to use the concise macro API, which cannot dynamically accept user input for paths.
+    if let Some(path_item) = openapi.paths.get_path_item(PLACEHOLDER_RESOLVER_PATH).cloned() {
+        openapi.paths.paths.insert(path.to_string(), path_item.clone());
+        openapi.paths.paths.remove(PLACEHOLDER_RESOLVER_PATH);
+    }
+
+    DidResolverHttpBinding { router, openapi }
 }
 
-// #[utoipa::path(
-//     get,
-//     summary = "Resolves a W3C Decentralized Identifier (DID) according to the DID Resolution specification.",
-//     description = "This endpoint is fully compliant with the W3C DID Resolution specification. It returns a DID Resolution Result object, including metadata and the resolved DID Document, following the standard resolution process.",
-//     path = ApiDid::AXUM_PATH,
-//     tags = [tags::OP_INDEX],
-//     responses(
-//         (status = OK, description = "Successfully resolved the DID. Returns the DID Resolution Result.", body = ResolutionResult, content_type = "application/did-resolution"),
-//         (status = BAD_REQUEST, description = "The provided DID is invalid.", body = ResolutionResult, content_type = "application/did-resolution"),
-//         (status = NOT_FOUND, description = "The DID does not exist or not found.", body = ResolutionResult, content_type = "application/did-resolution"),
-//         (status = GONE, description = "The DID has been deactivated.", body = ResolutionResult, content_type = "application/did-resolution"),
-//         (status = INTERNAL_SERVER_ERROR, description = "An unexpected error occurred during resolution.", body = ResolutionResult, content_type = "application/did-resolution"),
-//         (status = NOT_IMPLEMENTED, description = "A functionality is not implemented.", body = ResolutionResult, content_type = "application/did-resolution"),
-//     ),
-//     params(
-//         ("did" = Did, Path, description = "The Decentralized Identifier (DID) to resolve.")
-//     ),
-// )]
-pub async fn did_resolver_endpoint(
-    Path(did): Path<String>,
-    state: State<DidResolverStateDyn>,
-) -> HttpBinding<ResolutionResult> {
+#[utoipa::path(
+    get,
+    summary = "Resolves a W3C Decentralized Identifier (DID) according to the DID Resolution specification.",
+    description = "This endpoint is fully compliant with the W3C DID Resolution specification. It returns a DID Resolution Result object, including metadata and the resolved DID Document, following the standard resolution process.",
+    path = PLACEHOLDER_RESOLVER_PATH,
+    responses(
+        (status = OK, description = "Successfully resolved the DID. Returns the DID Resolution Result.", body = ResolutionResult, content_type = "application/did-resolution"),
+        (status = BAD_REQUEST, description = "The provided DID is invalid.", body = ResolutionResult, content_type = "application/did-resolution"),
+        (status = NOT_FOUND, description = "The DID does not exist or not found.", body = ResolutionResult, content_type = "application/did-resolution"),
+        (status = GONE, description = "The DID has been deactivated.", body = ResolutionResult, content_type = "application/did-resolution"),
+        (status = INTERNAL_SERVER_ERROR, description = "An unexpected error occurred during resolution.", body = ResolutionResult, content_type = "application/did-resolution"),
+        (status = NOT_IMPLEMENTED, description = "A functionality is not implemented.", body = ResolutionResult, content_type = "application/did-resolution"),
+    ),
+    params(
+        ("did" = Did, Path, description = "The Decentralized Identifier (DID) to resolve.")
+    ),
+)]
+pub async fn did_resolver(Path(did): Path<String>, state: State<DidResolverStateDyn>) -> HttpBinding<ResolutionResult> {
     let resolver = &state.resolver;
     let parsed_did = match Did::from_str(&did) {
         Ok(did) => did,
