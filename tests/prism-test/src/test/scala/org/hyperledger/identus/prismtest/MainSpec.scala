167e41b790f32baa8f69ec1374a27167afcde90a
Pat Losoponkul <patextreme@hotmail.com>
2025-10-28 19:40:24 +0700

    test: add did fixture generation and fix test operations
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- tests/prism-test/src/test/scala/org/hyperledger/identus/prismtest/MainSpec.scala
+++ tests/prism-test/src/test/scala/org/hyperledger/identus/prismtest/MainSpec.scala
@@ -7,6 +7,7 @@ import org.hyperledger.identus.prismtest.suite.DeactivateStorageOperationSuite
 import org.hyperledger.identus.prismtest.suite.UpdateDidOperationSuite
 import org.hyperledger.identus.prismtest.suite.UpdateStorageOperationSuite
 import org.hyperledger.identus.prismtest.utils.TestUtils
+import proto.prism_ssi.KeyUsage
 import zio.*
 import zio.http.Client
 import zio.test.*
@@ -41,7 +42,33 @@ object MainSpec extends ZIOSpecDefault, TestUtils:
     //     NodeName.layer("scala-did")
     //   )
 
-    (neoprismSpec + prismNodeSpec).provide(Runtime.removeDefaultLoggers)
+    (neoprismSpec + prismNodeSpec + generateDidFixtureSpec).provide(Runtime.removeDefaultLoggers)
       @@ TestAspect.timed
       @@ TestAspect.withLiveEnvironment
       @@ TestAspect.parallelN(1)
+
+  // Comment the ignore aspect and run `sbt testOnly -- -tags fixture`
+  // to output the generated test vector
+  private def generateDidFixtureSpec = test("generate did fixtures for testing") {
+    val seed = Array.fill[Byte](64)(0)
+    val vdrKeyName = "vdr-0"
+    val makeVdrKey = KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'"
+
+    val spo = builder(seed).createDid
+      .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+      .key(vdrKeyName)(makeVdrKey)
+      .build
+      .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+
+    val spoHex = spo.toByteArray.toHexString
+    val did = spo.getDid.get
+    val (_, vdrHdKey) = makeVdrKey(seed)
+    val vdrPrivateKeyHex = vdrHdKey.getKMMSecp256k1PrivateKey().getEncoded().toHexString
+
+    for
+      _ <- ZIO.debug(s"DID                : $did")
+      _ <- ZIO.debug(s"VDR key name       : $vdrKeyName")
+      _ <- ZIO.debug(s"VDR privateKey hex : $vdrPrivateKeyHex")
+      _ <- ZIO.debug(s"SignOperation hex  : $spoHex")
+    yield assertCompletes
+  } @@ TestAspect.tag("fixture") @@ TestAspect.ignore
