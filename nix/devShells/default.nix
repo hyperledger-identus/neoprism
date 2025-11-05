2cefa52a08fc5198c4038e2704ff2ad6f6f978af
Pat Losoponkul <patextreme@hotmail.com>
2025-11-05 17:27:24 +0700

    feat: add prism-test development shell environment
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -3,6 +3,7 @@
 {
   default = import ./neoprism.nix { inherit pkgs; };
   cardano = import ./cardano.nix { inherit pkgs; };
+  prism-test = import ./prism-test.nix { inherit pkgs; };
 
   docs = import ./docs.nix { inherit pkgs self; };
 }
