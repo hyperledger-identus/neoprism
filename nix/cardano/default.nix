34c1faa2fd303ecc1c0d1a6c722931ed4e48a25c
Pat Losoponkul <patextreme@hotmail.com>
2026-01-21 01:01:35 +0700

    Consolidate devShell definitions
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/cardano/default.nix
+++ nix/cardano/default.nix
@@ -27,5 +27,29 @@
               tagSuffix = "-arm64";
             };
       };
+
+      devShells.cardano = pkgs.mkShell {
+        packages = with pkgs; [
+          nix
+          jq
+          hurl
+          cardano-node
+          cardano-cli
+          cardano-wallet
+          cardano-testnet
+          cardano-db-sync
+          cardano-submit-api
+        ];
+
+        shellHook = ''
+          export ROOT_DIR=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
+          ${pkgs.cowsay}/bin/cowsay "Working on project root directory: $ROOT_DIR"
+          cd "$ROOT_DIR"
+        '';
+
+        CARDANO_CLI = "${pkgs.cardano-cli}/bin/cardano-cli";
+        CARDANO_NODE = "${pkgs.cardano-node}/bin/cardano-node";
+        PRISM_HOME = ".";
+      };
     };
 }
