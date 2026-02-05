c753bd2130dd0aac489b397997259122a3e7e678
patextreme <patextreme@users.noreply.github.com>
2026-02-05 08:59:56 +0000

    deploy: 897ed11fefe2537bfae5708f0af45bf589a58e87

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0b5d80d9.js',
+                path_to_root + 'searchindex-42f71dc7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
