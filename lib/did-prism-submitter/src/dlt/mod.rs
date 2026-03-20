33d7dbeb252b955b1ae41bdb435121307c35ab00
Pat Losoponkul <patextreme@hotmail.com>
2026-03-20 15:40:51 +0700

    Add embedded-wallet feature flag to did-prism-submitter

--- lib/did-prism-submitter/src/dlt/mod.rs
+++ lib/did-prism-submitter/src/dlt/mod.rs
@@ -1,2 +1,12 @@
+//! DLT submission implementations.
+//!
+//! Features `cardano-wallet` and `embedded-wallet` provide alternative submitter
+//! implementations and are mutually exclusive at runtime. Only one should be
+//! enabled when building the node binary.
+
 #[cfg(feature = "cardano-wallet")]
 pub mod cardano_wallet;
+
+// TODO: US-008 will add embedded_wallet module
+// #[cfg(feature = "embedded-wallet")]
+// pub mod embedded_wallet;
